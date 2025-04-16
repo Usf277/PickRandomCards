51a16e61f19e14c4a85ce1bcc54a667a7f5b121a
Youssef Safwat <youssefsafwat77@gmail.com>
2025-04-16 17:37:31 +0200

    Initial Commit

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The Number Of Cards To Pick : ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line,out int numberOfCards))
+            {
+                foreach (string card  in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Plz Enter A Valid Number : ");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
